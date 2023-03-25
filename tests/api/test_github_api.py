4a6fdd3b60a3a5ce97dd9e890d2c455c047887a5
lozoviy_artur <lozoviy.artur@gmail.com>
2023-03-25 11:09:37 +0200

    modules/api/clients/github.py

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -11,3 +11,21 @@ def test_user_exists(github_api):
 def test_user_not_exists(github_api):
     r = github_api.get_user('butenkosergii12323')
     assert r['message'] == 'Not Found'
+
+@pytest.mark.api
+def test_repo_can_be_found(github_api):
+    r = github_api.search_repo("qa_automation")
+    number_of_qa_auto_accounts = (r['total_count'])
+    assert number_of_qa_auto_accounts > 1000
+    print("\ntotal qa_automation accounts:" + str(number_of_qa_auto_accounts))
+
+def test_repo_not_exist(github_api):
+    r = github_api.search_repo("abracadabra_just_account")
+    total_accounts = (r['total_count'])
+    assert total_accounts == 0
+    print("\ntotal abracadabra_just_account accounts:" + str(total_accounts))
+
+
+
+
+
